4a08386140e454c0f31511a8d0f74e59fe0e3200
wiertlooo <watulewicz@gmail.com>
2024-01-17 16:14:22 +0100

    new component: navbar template

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
